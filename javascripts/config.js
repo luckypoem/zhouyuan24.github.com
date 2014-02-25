40f8645cb634c0ee8ba5021d74bf67f8994dc2d6
zhouyuan <zhouyuan24@gmail.com>
2014-02-25 11:26:55 +0800

    first commit

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,5 +1,5 @@
 var disqus_shortname = '周渊的博客';
-var hostbase = 'http://zhouyuan24.github.com';
+var hostbase = 'http://zhouyuan24.github.io';
 var githubname = 'zhouyuan24';
 var repos = 'zhouyuan24.github.io';
 var sitetitle = '周渊的博客';
