202325610bf553696b118444d7dd48174db00374
zhouyuan <zhouyuan24@gmail.com>
2014-09-02 10:57:25 +0800

    gitbook

--- javascripts/config.js
+++
@@ -1,6 +0,0 @@
-var disqus_shortname = 'zhouyuan24';
-var hostbase = 'http://zhouyuan24.github.com';
-var githubname = 'zhouyuan24';
-var repos = 'zhouyuan24.github.com';
-var sitetitle = '周渊的博客';
-var rss = '';
